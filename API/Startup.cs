5e8df1b263e95bc4c77e1969a52ef29201f0207b
laxman83 <laxmankavale83@gmail.com>
2022-04-14 17:00:33 +0530

    Create project from scratch

---
+++ API/Startup.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public class Startup
+        
+    {
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            _ = services.AddDbContext<DataContext>(options =>
+              {
+                  _ = options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+              });
+            _ = services.AddControllers();
+            _ = services.AddSwaggerGen(c =>
+              {
+                  c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+              });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                _ = app.UseDeveloperExceptionPage();
+                _ = app.UseSwagger();
+                _ = app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+            }
+
+            _ = app.UseHttpsRedirection();
+
+            _ = app.UseRouting();
+
+            _ = app.UseAuthorization();
+
+            _ = app.UseEndpoints(endpoints =>
+              {
+                  _ = endpoints.MapControllers();
+              });
+        }
+    }
+}
